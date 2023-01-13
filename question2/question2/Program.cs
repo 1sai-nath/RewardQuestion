0808f9d8f95d4240496b7b56f5e8227a3e6f3480
1sai-nath <121101770+1sai-nath@users.noreply.github.com>
2023-01-13 20:58:46 +0530

    Add files via upload
    Write an abstract class called Shape containing  string shapeName as data member and and two abstract methods named findArea() and findPerimeter(). Develop two classes named Circle and Rectangle, inherited from Shape and these classes should contain the required data members.  Also, these classes are to override both the methods.

---
+++ question2/question2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace question2
+{
+    internal class Program
+    {
+        abstract class Shape
+        {
+            public string shapeName;
+
+            public abstract double findArea();
+            public abstract double findPerimeter();
+        }
+
+        class Circle : Shape
+        {
+            public double radius;
+
+            public Circle(double r)
+            {
+                radius = r;
+                shapeName = "Circle";
+            }
+
+            public override double findArea()
+            {
+                return 3.14 * radius * radius;
+            }
+
+            public override double findPerimeter()
+            {
+                return 2 * 3.14 * radius;
+            }
+        }
+
+        class Rectangle : Shape
+        {
+            public double length, width;
+
+            public Rectangle(double l, double w)
+            {
+                length = l;
+                width = w;
+                shapeName = "Rectangle";
+            }
+
+            public override double findArea()
+            {
+                return length * width;
+            }
+
+            public override double findPerimeter()
+            {
+                return 2 * (length + width);
+            }
+        }
+
+        class MainClass
+        {
+            public static void Main(string[] args)
+            {
+                Circle circle = new Circle(5);
+                Console.WriteLine("Area of " + circle.shapeName + ": " + circle.findArea());
+                Console.WriteLine("Perimeter of " + circle.shapeName + ": " + circle.findPerimeter());
+
+                Rectangle rectangle = new Rectangle(5, 10);
+                Console.WriteLine("Area of " + rectangle.shapeName + ": " + rectangle.findArea());
+                Console.WriteLine("Perimeter of " + rectangle.shapeName + ": " + rectangle.findPerimeter());
+            }
+        }
+    }
+}
